e5e02edc15a4d3442421617c6751d796e9726d91
Raffaele Raggi <raffaeleraggi@gmail.com>
2026-05-30 10:31:05 +0200

    fix spa routing

---
+++ backend/src/main/java/org/wedding/rsvp/controller/SpaController.java
@@ -0,0 +1,16 @@
+package org.wedding.rsvp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpaController {
+
+    @RequestMapping(value = {
+            "/admin",
+            "/i/**"
+    })
+    public String forwardReactRoutes() {
+        return "forward:/index.html";
+    }
+}
