c1ca994d9c5f04446d2b3e2d6c191907217d05fa
Raffaele Raggi <raffaeleraggi@gmail.com>
2026-06-10 22:44:03 +0200

    fix landing dashboard style

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/global.css";
 
+document.fonts.ready.then(() => {
+  document.documentElement.classList.add("fonts-loaded");
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
